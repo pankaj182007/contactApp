c725978ce1975a033d4992e7c078c1c47bb49a30
Pushpendra singh <pankaj182007@gmail.com>
2024-06-14 13:46:43 +0530

    git commit

--- src/main/java/org/contactApp/dao/ContactDaoImpl.java
+++ src/main/java/org/contactApp/dao/ContactDaoImpl.java
@@ -1,8 +1,11 @@
 package org.contactApp.dao;
 
 import org.contactApp.entity.Contacts;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ContactDaoImpl implements ContactDao {
+
+    public JdbcTemplate jdbcTemplate;
     @Override
     public int addContact(Contacts contacts) {
         return 0;
